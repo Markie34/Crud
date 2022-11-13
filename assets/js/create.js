84f1e95094dc8945dbbe45db5a529e4856c73327
Markie34 <66825780+Markie34@users.noreply.github.com>
2022-11-13 03:12:06 -0800

    Add files via upload

---
+++ assets/js/create.js
@@ -0,0 +1,80 @@
+
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["ID"] = document.getElementById("ID").value;
+    formData["fname"] = document.getElementById("fname").value;
+    formData["lname"] = document.getElementById("lname").value;
+    formData["course"] = document.getElementById("course").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("studentRec").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.ID;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.fname;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.lname;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.course;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("ID").value="";
+    document.getElementById("fname").value="";
+    document.getElementById("lname").value="";
+    document.getElementById("course").value="";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("ID").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("fname").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("lname").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("course").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.ID;
+    selectedRow.cells[1].innerHTML = formData.fname;
+    selectedRow.cells[2].innerHTML = formData.lname;
+    selectedRow.cells[3].innerHTML = formData.course;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("studentRec").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("fname").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
